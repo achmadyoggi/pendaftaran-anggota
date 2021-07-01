69501fa76414f70de80d2d80962f6aaed0b68cac
achmadyoggi <achmadyoggi@gmail.com>
2021-07-01 17:25:07 +0700

    database

---
+++ db_pendaftaran.sql
@@ -0,0 +1,102 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 30 Jun 2021 pada 19.44
+-- Versi server: 10.4.14-MariaDB
+-- Versi PHP: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_pendaftaran`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_admin`
+--
+
+CREATE TABLE `tb_admin` (
+  `id_admin` int(11) NOT NULL,
+  `nm_admin` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_admin`
+--
+
+INSERT INTO `tb_admin` (`id_admin`, `nm_admin`, `username`, `password`) VALUES
+(1, 'Achmad', 'admin', '21232f297a57a5a743894a0e4a801fc3'),
+(2, 'Yu Zhong', '1', 'c4ca4238a0b923820dcc509a6f75849b');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_pendaftaran`
+--
+
+CREATE TABLE `tb_pendaftaran` (
+  `id_pendaftaran` char(10) NOT NULL,
+  `tgl_daftar` date NOT NULL,
+  `th_ajaran` varchar(9) NOT NULL,
+  `jurusan` varchar(50) NOT NULL,
+  `nm_peserta` varchar(50) NOT NULL,
+  `tmp_lahir` varchar(50) NOT NULL,
+  `tgl_lahir` date NOT NULL,
+  `jk` enum('Laki-Laki','Perempuan') NOT NULL,
+  `agama` varchar(15) NOT NULL,
+  `almt_peserta` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_pendaftaran`
+--
+
+INSERT INTO `tb_pendaftaran` (`id_pendaftaran`, `tgl_daftar`, `th_ajaran`, `jurusan`, `nm_peserta`, `tmp_lahir`, `tgl_lahir`, `jk`, `agama`, `almt_peserta`) VALUES
+('P202100001', '2021-06-30', '2021/2022', 'Teknik Informatika', 'adinda', 'Malang', '2002-03-27', 'Perempuan', 'Islam', 'Jl. Jalan'),
+('P202100002', '2021-06-30', '2021/2022', 'Teknik Sipil', 'Achmad Yoggi Setiawan', 'Jember', '2001-02-23', 'Laki-Laki', 'Protestan', 'Jl. Yuk'),
+('P202100003', '2021-06-30', '2021/2022', 'Teknik Mesin', 'Onni', 'Malang', '2001-10-03', 'Laki-Laki', 'Islam', 'Jl. Kemana');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tb_admin`
+--
+ALTER TABLE `tb_admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indeks untuk tabel `tb_pendaftaran`
+--
+ALTER TABLE `tb_pendaftaran`
+  ADD PRIMARY KEY (`id_pendaftaran`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_admin`
+--
+ALTER TABLE `tb_admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
